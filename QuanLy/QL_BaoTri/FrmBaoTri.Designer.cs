da4f25fc1fc5c9cad3c79ba18ce3c8043928b902
an le <antuanle2002@gmail.com>
2024-06-10 22:31:29 +0700

    Add project files.

---
+++ QuanLy/QL_BaoTri/FrmBaoTri.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace QuanLy.QL_CongViec
+{
+    partial class FrmBaoTri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.dtgvBt = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayCap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ViTri = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvBt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtgvBt);
+            this.panel2.Controls.Add(this.btnTim);
+            this.panel2.Controls.Add(this.txtTimKiem);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(928, 488);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // btnTim
+            // 
+            this.btnTim.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(255)))));
+            this.btnTim.ForeColor = System.Drawing.Color.White;
+            this.btnTim.Location = new System.Drawing.Point(795, 10);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(86, 30);
+            this.btnTim.TabIndex = 17;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = false;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(52, 10);
+            this.txtTimKiem.Multiline = true;
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(723, 30);
+            this.txtTimKiem.TabIndex = 16;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // dtgvBt
+            // 
+            this.dtgvBt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvBt.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Ten,
+            this.NgayCap,
+            this.ViTri});
+            this.dtgvBt.Location = new System.Drawing.Point(6, 61);
+            this.dtgvBt.Name = "dtgvBt";
+            this.dtgvBt.Size = new System.Drawing.Size(916, 424);
+            this.dtgvBt.TabIndex = 19;
+            // 
+            // Id
+            // 
+            this.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Id.HeaderText = "Mã thiết bị";
+            this.Id.Name = "Id";
+            // 
+            // Ten
+            // 
+            this.Ten.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Ten.HeaderText = "Tên thiết bị";
+            this.Ten.Name = "Ten";
+            // 
+            // NgayCap
+            // 
+            this.NgayCap.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NgayCap.HeaderText = "Ngày cấp";
+            this.NgayCap.Name = "NgayCap";
+            // 
+            // ViTri
+            // 
+            this.ViTri.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ViTri.HeaderText = "Vị trí";
+            this.ViTri.Name = "ViTri";
+            // 
+            // FrmBaoTri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 490);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmBaoTri";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvBt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.DataGridView dtgvBt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgayCap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ViTri;
+    }
+}
---
+++ QuanLy/QL_BaoTri/FrmBaoTri.cs
@@ -0,0 +1,84 @@
+﻿using DevExpress.Xpo.DB.Helpers;
+using QuanLyChungCu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq.Mapping;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLy.QL_CongViec
+{
+    public partial class FrmBaoTri : Form
+    {
+        
+        public FrmBaoTri()
+        {
+            InitializeComponent();
+            FillTable("select * from ThietBi where TinhTrang = N'Bị hỏng'");
+        }
+        public void FillTable(string query)
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                DataTable table = new DataTable();
+                using (SqlConnection sqlConnection = ConnectDb.GetConnection())
+                {
+                    sqlConnection.Open();
+                    adapter = new SqlDataAdapter(query, sqlConnection);
+                    adapter.Fill(table);
+                    sqlConnection.Close();
+                }
+                dtgvBt.Columns["Id"].DataPropertyName = "Id";
+                dtgvBt.Columns["Ten"].DataPropertyName = "Ten";
+                dtgvBt.Columns["NgayCap"].DataPropertyName = "NgayCap";
+                dtgvBt.Columns["ViTri"].DataPropertyName = "ViTri";
+
+                //dtgvBt.Columns["Id"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                //dtgvBt.Columns["MaTB"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                //dtgvBt.Columns["NgayBT"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                //dtgvBt.Columns["NgayKT"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dtgvBt.AutoGenerateColumns = false;
+                dtgvBt.DataSource = null;
+                dtgvBt.DefaultCellStyle.ForeColor = Color.Black;
+                dtgvBt.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dtgvBt.MultiSelect = false;
+                dtgvBt.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Tab_DanhSach_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            FillTable("select * from BaoTri where Id = '"+txtTimKiem.Text+"'");
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
